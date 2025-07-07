d87022284e0bf08482bb634e354c9fda0957127e
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-07-07 22:49:04 +0700

    Update userAgents.js

--- skw/userAgents.js
+++ skw/userAgents.js
@@ -1,4 +1,4 @@
-export default [
+export const userAgents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edge/91.0.864.64',
     'Mozilla/5.0 (Windows NT 6.1; WOW64) Gecko/20100101 Firefox/54.0',
